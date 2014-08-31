cddd69692eec81a2f4a2413f43a902e9719c9204
Ben Tatman <ben@tatmans.co.uk>
2014-08-31 09:33:25 +0100

    All code seems to work, takes into account gravity, initial velocity blah blah, need to work on some code to set up particles now.

--- control.c
+++ control.c
@@ -12,6 +12,8 @@ typedef struct {
 	float mass;
 } particle;
 
+float time_skip = 1;
+
 float abs_r(float p) {
 	if (p < 0)
 		return 0-p;
@@ -44,6 +46,25 @@ e_vector gravity_calculate(particle a, particle b) {
 	return r;
 }
 
+int print_e_vector(e_vector p) {
+	printf("%f %f %f\n", p.x, p.y, p.z);
+	return 1;
+}
+
+e_vector compute_velocity(e_vector vel, e_vector accel[], int acceli) {
+	e_vector p = vel;
+	print_e_vector(vel);
+	int i;
+	for (i = 0; i < acceli; i++) {
+		p.x += accel[i].x * time_skip;
+		p.y += accel[i].y * time_skip;
+		p.z += accel[i].z * time_skip;
+		print_e_vector(accel[i]);
+	}
+	print_e_vector(p);
+	return p;
+}
+
 int main(int argc, char * argv[]) {
 	particle a;
 	particle b;
@@ -51,14 +72,16 @@ int main(int argc, char * argv[]) {
 	a.location.y = 0;
 	a.location.z = 0;
 	b.location.x = -10;
-	b.location.y = -10;
-	b.location.z = 0;
+	b.location.y = 0;
+	b.location.z = 10;
 	a.mass =  10000000000;
 	b.mass = 1000000;
 	
 	e_vector q;
 	q = gravity_calculate(a,b);
 	printf("%f %f %f\n", q.x, q.y, q.z);
+	e_vector p[] = {q, a.velocity};
+	compute_velocity(a.location, p, 2);
 	return 1;
 }
 
