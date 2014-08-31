8fe05debf9e8a5dc8b9d72106be6719161c7baa9
Ben Tatman <ben@tatmans.co.uk>
2014-08-31 13:57:58 +0100

    Working on generating particles.

--- control.c
+++ control.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
 #define G 1
 
 typedef struct {
@@ -36,6 +39,9 @@ e_vector gravity_calculate(particle a, particle b) {
 	q.z = b.location.z - a.location.z; // Basically, we just get the distance between them as e_vector. This is relative to a - a + q = b.
 	float distance = magnitude(a.location, b.location);
 	printf("D %f\n", distance);
+	if (distance == 0) {
+		distance = 0.001;
+	}
 	float grav = (G*a.mass*b.mass)/(distance*distance);
 	float grav_accel = grav / a.mass; // solving for a, we have the force between so we want acceleration on a.
 	float total_vec = abs_r(q.x) + abs_r(q.y) + abs_r(q.z);
@@ -65,7 +71,58 @@ e_vector compute_velocity(e_vector vel, e_vector accel[], int acceli) {
 	return p;
 }
 
+int print_particle_set(particle p[], int pl, float time, float max_time) {
+	printf("= %3f%% ========= %4f seconds ===============\n", 100*(time/max_time), time);
+	int i;
+	for (i = 0; i < pl; i++) {
+		printf("%d: %f/%f/%f - %f/%f/%f\n", i, p[i].location.x, p[i].location.y, p[i].location.z, p[i].velocity.x, p[i].velocity.y, p[i].velocity.z);
+	}
+	return 1;
+}
+
+particle generate_particle(void) {
+	particle q;
+	q.mass = 100;
+	q.location.x = 0;
+	q.location.y = 0;
+	q.location.z = 0;
+	q.velocity.x = (rand()%10)-5;
+	q.velocity.y = (rand()%10)-5;
+	q.velocity.z = (rand()%10)-5;
+	return q;
+}
+
 int main(int argc, char * argv[]) {
+	printf("%d: %s\n", argc, argv[0]);
+	/* 
+		1: name
+		2: no particles
+		3: timeswap
+		4: timebase
+		5: timelength
+	*/
+	
+
+
+	srand(time(NULL));
+
+
+
+	if (argc < 5) {
+		printf("unisim [number of particles] [time] [printtime] [length to run]\n");
+		return 1;
+	}
+	particle a[atoi(argv[1])];
+	
+	int i,p;
+	for (i = 0; i < atoi(argv[1]); i++) {
+		a[i] = generate_particle();
+	}
+	print_particle_set(a, atoi(argv[1]), 3, 6);
+	
+	
+	
+	/*
 	particle a;
 	particle b;
 	a.location.x = 0;
@@ -82,6 +139,6 @@ int main(int argc, char * argv[]) {
 	printf("%f %f %f\n", q.x, q.y, q.z);
 	e_vector p[] = {q, a.velocity};
 	compute_velocity(a.location, p, 2);
-	return 1;
+*/	return 1;
 }
 
